aef47c1688466655305686cfd69caa101693c751
Fadli1009 <argyfa3@gmail.com>
2024-01-04 06:01:14 +0700

    install template

--- resources/views/template/base.blade.php
+++ resources/views/template/base.blade.php
@@ -282,14 +282,7 @@
 
             <!-- page content -->
             <div class="right_col" role="main">
-                <div class="">
-                    <div class="page-title">
-                        <div class="title_left">
-                            <h3>Fixed Sidebar <small> Just add class <strong>menu_fixed</strong></small></h3>
-                        </div>
-                    </div>
-                    @yield('content')
-                </div>
+                @yield('content')
             </div>
             <!-- /page content -->
 
